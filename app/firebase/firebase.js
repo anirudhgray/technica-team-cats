3460a13499b10167e708a9ee55ac8a082bff78de
SayarB <sayarbhattacharyya03@gmail.com>
2022-05-21 18:58:30 +0530

    firestore wallet address check

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,9 @@ const firebaseConfig = {
   projectId: "technica-team-cats",
   storageBucket: "technica-team-cats.appspot.com",
   messagingSenderId: "96834032162",
-  appId: "1:96834032162:web:5789e6d9e700ca0aa7707b"
+  appId: "1:96834032162:web:5789e6d9e700ca0aa7707b",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
