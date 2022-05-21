8edf177831716aeb850b6debf734343d47f202d8
anirudh <anirudh04mishra@gmail.com>
2022-05-21 16:44:10 +0530

    feat: Add basic MetaMask connection.

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import { PharmaProvider } from '../context/PharmaContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <PharmaProvider><Component {...pageProps} /></PharmaProvider>
 }
 
 export default MyApp
