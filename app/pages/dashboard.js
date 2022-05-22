ec9c5b8e5dce88319257c847d225d8af37e18d7d
anirudh <anirudh04mishra@gmail.com>
2022-05-22 07:46:00 +0530

    Fix crossorigin

--- app/pages/dashboard.js
+++ app/pages/dashboard.js
@@ -3,9 +3,23 @@ import styles from "../styles/Dashboard.module.css";
 import { useContext } from "react";
 import { PharmaContext } from "../context/PharmaContext";
 import Link from 'next/link';
+import axios from 'axios'
 export default function Dashboard() {
   const { appStatus, currentAccount, connectToWallet, email, firstName, lastName, type } =
     useContext(PharmaContext);
+
+  const checkBatch = async (e) => {
+    e.preventDefault()
+    const url = 'https://pharmatrue.vercel.app/get_med/allegra'
+    await axios
+    .get(url)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err.data)
+    })
+  }
 
   return (
     <div className={styles.container}>
@@ -16,7 +30,7 @@ export default function Dashboard() {
         <p className={styles.white}>{email} ({type})</p>
       </header>
       <div className={styles.main}>
-        <form className={styles.centered}>
+        <form onSubmit={checkBatch} className={styles.centered}>
           <input placeholder='Batch to check'></input>
           <button type='submit'>Check</button>
         </form>
